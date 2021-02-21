130ad4054c960b3f98260d84498a35a219054df7
obmeir <orenbm11@gmail.com>
2021-02-21 06:20:38 -0500

    fixed

---
+++ Scripts/Migrations/Board/V1611217472_only_boardcol_fk_cascade.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRANSACTION [Tran1611217472]
+
+    BEGIN TRY
+
+        ALTER TABLE [dbo].[task_cards]   
+            DROP CONSTRAINT [fk_task_card_task_board_id_ref_task_board_id]
+            
+        ALTER TABLE [dbo].[task_cards]
+            DROP COLUMN [task_board_id]
+
+        COMMIT TRANSACTION [Tran1611217472]
+
+    END TRY
+
+    BEGIN CATCH
+
+        ROLLBACK TRANSACTION [Tran1611217472]
+
+    END CATCH  
+
+ Go
